490c4a6a3ce0bfdbb133f3b35abd3d86ec30ad2f
Brad Torrence <brad.torrence@ku.edu>
2014-06-26 09:59:10 -0500

    Added ANSI terminal activity to Console.hs

--- Life/Console.hs
+++ Life/Console.hs
@@ -2,7 +2,21 @@ module Life.Console where
 
 import Life.Types
 import System.Console.ANSI
+import Control.Concurrent
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+goto :: Pos -> IO ()
+goto (x,y) =  putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do 	goto p
+					putStr xs
 
 lifeConsole :: Life board => board -> IO ()
-lifeConsole = error ""
+lifeConsole b = do	cls
+					sequence_ [writeat p "O" | p <- alive b]
+					threadDelay (50 * 1000)
+					lifeConsole (nextgen b)
 
