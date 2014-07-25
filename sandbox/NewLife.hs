a7653e8194c48743129d062f58f2c92f3b23a6f4
Brad Torrence <brad.torrence@ku.edu>
2014-07-25 12:30:45 -0500

    completed most of Hermit conversion scripts

--- sandbox/NewLife.hs
+++ sandbox/NewLife.hs
@@ -4,7 +4,7 @@ module NewLife where
 -- Libraries required for Hermit transformations
 import Life.Types
 import Data.Set as Set
-import Data.List (sort)
+import Data.List (sort,nub)
 
 -- Standard implementation
 type Board = LifeBoard [Pos]
@@ -26,16 +26,20 @@ instance Life Board' where
 -- Transformations required by hermit for worker/wrapper conversions
 
 -- repb and absb change the underlying board field
+{-# NOINLINE repb #-}
 repb :: [Pos] -> Set Pos
 repb = fromDistinctAscList
 
+{-# NOINLINE absb #-}
 absb :: Set Pos -> [Pos]
 absb = toAscList
 
 -- repB and absB change the entire Board structure
+{-# NOINLINE repB #-}
 repB :: Board -> Board'
 repB b = LifeBoard (config b) $ repb (board b)
 
+{-# NOINLINE absB #-}
 absB :: Board' -> Board
 absB b = LifeBoard (config b) $ absb (board b)
 
@@ -102,28 +106,15 @@ absBB f = absB . f . repB
 {-# RULES "repb-filter" [~] forall f b. repb (Prelude.filter f b) = Set.filter f (repb b) #-}
 {-# RULES "repb-map" [~] forall f b. repb (sort (Prelude.map f b)) = Set.map f (repb b) #-}
 
-{-# RULES "absPb-fun" [~] forall f. absPb f = absb . f #-}
-{-# RULES "repPb-fun" [~] forall f. repPb f = repb . f #-}
-
-{-# RULES "absCPb-fun" [~] forall f c. absCPb f c = absPb (f c) #-}
 {-# RULES "config-absB" [~] forall b. config (absB b) = config b #-}
 {-# RULES "repB-absB" [~] forall b. repB (absB b) = b #-}
 {-# RULES "filter-absb" [~] forall f b. Prelude.filter f (absb b) = absb (Set.filter f b) #-}
 {-# RULES "length-absb" [~] forall b. length (absb b) = size b #-}
 
-{-# RULES "repBb-fun" [~] forall f. repBb f = repb . f . absB #-}
+{-# RULES "repb-absb" [~] forall b. repb (absb b) = b #-}
 
-{-
-{-# RULES "repb-filter-absb" [~] forall f b. repb (Prelude.filter f (absb b)) = Set.filter f b #-}
-{-# RULES "repb-map-absb" [~] forall f b. repb (sort (Prelude.map f (absb b))) = Set.map f b #-}
+{-# RULES "ncm-absb" [~] forall f b. nub (concatMap (\p -> absb (f p)) (absb b)) = absb (unions (toList(Set.map (\p -> f p) b))) #-}
 
-{-# RULES "length-absb" [~] forall b. length . Prelude.filter (isAlive (absB b)) . (neighbs (config (absB b))) = size . Set.filter (isAlive b) . (neighbs (config b)) #-}
-
-{-# RULES "survivors" [~] forall b. [ p | p <- board (absB b), elem (liveneighbs (absB b) p) [2,3] ] = Set.filter (\p -> elem (liveneighbs b p) [2,3]) (board b) #-}
-
-{-# RULES "births" [~] forall b. [ p | p <- nub (concat (Prelude.map (neighbs (config (absB b))) (board (absB b)))), isEmpty (absB b) p, liveneighbs (absB b) p == 3 ] = Set.filter (\p -> (isEmpty b p) && (liveneighbs b p == 3)) (Set.foldr (\p s -> union s (neighbs (config b) p)) Set.empty (board b)) #-}
-
-{-# RULES "nextgen" [~] forall b. sort (survivors (absB b) ++ births (absB b)) = survivors b `union` births b #-}
--}
-
+{-# RULES "sort++absb" [~] forall b1 b2. sort (absb b1 ++ absb b2) = absb (union b1 b2) #-}
+{-# RULES "repB-LifeBoard-absb" [~] forall c b. repB (LifeBoard c (absb b)) = LifeBoard c b #-}
 
