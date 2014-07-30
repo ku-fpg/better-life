9526b092751f30fdc88a649d377e88d853406f75
Brad Torrence <brad.torrence@ku.edu>
2014-07-29 20:36:46 -0500

    added notes to hermit example

--- sandbox/HERMIT/Set/Life.hs
+++ sandbox/HERMIT/Set/Life.hs
@@ -78,21 +78,32 @@ absPBB f p = absB . (f p) . repB
 
 
 -- Rules for hermit conversion
+-- Rules that move abs and rep functions up/down the AST
 {-# RULES "repB-LifeBoard" [~] forall c b. repB (LifeBoard c b) = LifeBoard c (repb b) #-}
 {-# RULES "LifeBoard-absb" [~] forall c b. LifeBoard c (absb b) = absB (LifeBoard c b) #-}
 {-# RULES "board-absB"  [~] forall b. board (absB b) = absb (board b) #-}
 {-# RULES "config-absB" [~] forall b. config (absB b) = config b #-}
 {-# RULES "repB-absB" [~] forall b. repB (absB b) = b #-}
 
+-- Rules that convert list-based combinators into set-based combinators
+-- For conversion to LifeBoard.empty
 {-# RULES "repb-null" [~] forall c. LifeBoard c (repb []) = LifeBoard c Set.empty #-}
+-- For conversion to Set.notMember)
 {-# RULES "not-elem-absb" [~] forall p b. not (elem p (absb b)) = notMember p b #-}
+-- For conversion to Set.member
 {-# RULES "elem-absb" [~] forall p b. elem p (absb b) = member p b #-}
+-- For conversion to Set.size
 {-# RULES "length-absb" [~] forall b. length (absb b) = size b #-}
+-- For conversion to Set.filter
 {-# RULES "filter-absb" [~] forall f b. Prelude.filter f (absb b) = absb (Set.filter f b) #-}
-{-# RULES "sort-map-absb" [~] forall f b. sort (Prelude.map f (absb b)) = absb (Set.map f b) #-}
+-- For conversion to Set.union
 {-# RULES "sort-++-absb" [~] forall b1 b2. sort (absb b1 ++ absb b2) = absb (union b1 b2) #-}
+-- For nub-concatMap chain in births function
 {-# RULES "ncm-absb" [~] forall f b. nub (concatMap (\p -> absb (board (f p))) (absb b)) = absb (unions (toList (Set.map (\p -> board (f p)) b))) #-}
+-- For conversion to Set.\\
 {-# RULES "diff-absb" [~] forall b1 b2. absb b1 List.\\ absb b2 = absb (b1 Set.\\ b2) #-}
+-- For conversion to Set.insert
 {-# RULES "insertion" [~] forall b p. sort (p : absb b) = absb (insert p b) #-}
+-- For conversion to Set.delete
 {-# RULES "deletion" [~] forall b p. Prelude.filter ((/=) p) (absb b) = absb (delete p b) #-}
 
