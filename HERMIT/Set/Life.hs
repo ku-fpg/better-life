0ca21bc7f49240090c52b77670697cf90bac175e
Brad Torrence <brad.torrence@ku.edu>
2014-11-16 15:23:40 -0600

    updated Set scripts to use uniform processes

--- HERMIT/Set/Life.hs
+++ HERMIT/Set/Life.hs
@@ -1,9 +1,9 @@
 module HERMIT.Set.Life where
 
--- Libraries required for Hermit transformations
-import Life.Types
 import Data.Set as Set
 import Data.List as List (nub,(\\))
+
+import Life.Types
 
 -- Standard implementation
 type Board = LifeBoard Config [Pos]
@@ -30,76 +30,76 @@ repB b = LifeBoard (config b) $ repb (board b)
 absB :: Board' -> Board
 absB b = LifeBoard (config b) $ absb (board b)
 
--- representation of "dims", "alive", "isAlive", "isEmpty", "liveneighbs"
+-- rep for "dims", "alive", "isAlive", "isEmpty", "liveneighbs"
 repBx :: (Board -> a) -> Board' -> a
 repBx f = f . absB
 
--- abstraction of "dims", "alive", "isAlive", "isEmpty", "liveneighbs"
+-- abs for "dims", "alive", "isAlive", "isEmpty", "liveneighbs"
 absBx :: (Board' -> a) -> Board -> a
 absBx f = f . repB
 
--- representation of "empty"
+-- rep for "empty"
 repxB :: (a -> Board) -> a -> Board'
 repxB f = repB . f
 
--- abstraction of "empty"
+-- abs for "empty"
 absxB :: (a -> Board') -> a -> Board
 absxB f = absB . f
 
--- representation of (Pos -> Board -> Board) "inv"
+-- rep for "inv"
 repxBB :: (a -> Board -> Board) -> a -> Board' -> Board'
 repxBB f x = repB . (f x) . absB
 
--- abstraction of (Pos -> Board' -> Board') "inv"
+-- abs for "inv"
 absxBB :: (a -> Board' -> Board') -> a -> Board -> Board
 absxBB f x = absB . (f x) . repB
 
--- representation of (Board -> Board -> Board) "diff"
+-- rep for "diff"
 repBBB :: (Board -> Board -> Board) -> Board' -> Board' -> Board'
 repBBB f b = repB . (f (absB b)) . absB
 
--- abstraction of (Board' -> Board' -> Board') "diff"
+-- abs for "diff"
 absBBB :: (Board' -> Board' -> Board') -> Board -> Board -> Board
 absBBB f b = absB . (f (repB b)) . repB
 
--- representation of (Board -> Board) "births", "survivors", "nextgen", "next"
+-- rep for "births", "survivors", "nextgen", "next"
 repBB :: (Board -> Board) -> Board' -> Board'
 repBB f = repB . f . absB
 
--- abstraction of (Board' -> Board') "births", "survivors", "nextgen", "next"
+-- abs for "births", "survivors", "nextgen", "next"
 absBB :: (Board' -> Board') -> Board -> Board
 absBB f = absB . f . repB
 
 
 -- Rules for hermit conversion
-{-# RULES "empty-b" [~] repxB (\c -> LifeBoard c []) = (\c -> LifeBoard c Set.empty) #-}
+{-# RULES "empty-b" [~] forall c. repB (LifeBoard c []) = LifeBoard c Set.empty #-}
 
-{-# RULES "isAlive" [~] repBx (\b p -> elem p (board b)) = (\b p -> member p (board b)) #-}
+{-# RULES "isAlive" [~] forall b p. elem p (board (absB b)) = member p (board b) #-}
 
-{-# RULES "inv" [~] forall f. 
-	repxBB (\p b -> LifeBoard (config b) (if absBx f b p
-					then Prelude.filter ((/=) p) (board b) 
-					else p : (board b))) 
-	= (\p b -> LifeBoard (config b) (if f b p 
+{-# RULES "inv" [~] forall f b p. 
+	repB (LifeBoard (config (absB b)) (if f (repB (absB b)) p 
+					then Prelude.filter ((/=) p) (board (absB b)) 
+					else p : (board (absB b)))) 
+	= LifeBoard (config b) (if f b p 
 					then delete p (board b) 
-					else insert p (board b))) #-}
+					else insert p (board b)) #-}
 
-{-# RULES "alive" [~] repBx (\b -> (board b)) = (\b -> toList (board b)) #-}
+{-# RULES "alive" [~] forall b. board (absB b) = toList (board b) #-}
 
-{-# RULES "dims" [~] repBx (\b -> case (config b) of (s,w) -> s) = (\b -> case (config b) of (s,w) -> s) #-}
+{-# RULES "dims" [~] forall b. config (absB b) = config b #-}
 
-{-# RULES "diff-b" [~] repBBB (\b1 b2 -> LifeBoard (config b1) (board b1 List.\\ board b2)) = (\b1 b2 -> LifeBoard (config b1) (board b1 Set.\\ board b2)) #-}
+{-# RULES "diff-b" [~] forall b1 b2. repB (LifeBoard (config (absB b1)) (board (absB b1) List.\\ board (absB b2))) = LifeBoard (config b1) (board b1 Set.\\ board b2) #-}
 
-{-# RULES "isEmpty" [~] forall f. repBx (\b p -> not (f b p)) = (\b p -> notMember p (board b)) #-}
+{-# RULES "isEmpty" [~] forall f b p. not (f (absB b) p) = notMember p (board b) #-}
 
-{-# RULES "liveneighbs" [~] forall f1 f2. repBx (\b x -> length (Prelude.filter (absBx f1 b) (f2 (config b) x))) = (\b x -> length (Prelude.filter (f1 b) (f2 (config b) x))) #-}
+{-# RULES "liveneighbs" [~] forall f b x. length (Prelude.filter (f (repB (absB b))) x) = length (Prelude.filter (f b) x) #-}
 
-{-# RULES "survivors" [~] forall f n. repBB (\b -> LifeBoard (config b) (Prelude.filter (\p -> elem (absBx f b p) n) (board b)))  = (\b -> LifeBoard (config b) (Set.filter (\p -> elem (f b p) n) (board b))) #-}
+{-# RULES "survivors" [~] forall f b n. repB (LifeBoard (config (absB b)) (Prelude.filter (\p -> elem (f (repB (absB b)) p) n) (board (absB b))))  = LifeBoard (config b) (Set.filter (\p -> elem (f b p) n) (board b)) #-}
 
-{-# RULES "births" [~] forall f1 f2 f3 n. repBB (\b -> LifeBoard (config b) (Prelude.filter (\p -> absBx f1 b p && absBx f2 b p == n) (nub (concatMap (f3 (config b)) (board b))))) = (\b -> LifeBoard (config b) (Set.filter (\p -> f1 b p && f2 b p == n) (unions (toList (Set.map (fromList . (f3 (config b))) (board b)))))) #-}
+{-# RULES "births" [~] forall f1 f2 f3 b n. repB (LifeBoard (config (absB b)) (Prelude.filter (\p -> f1 (repB (absB b)) p && f2 (repB (absB b)) p == n) (nub (concatMap (f3 (config (absB b))) (board (absB b)))))) = LifeBoard (config b) (Set.filter (\p -> f1 b p && f2 b p == n) (unions (toList (Set.map (fromList . (f3 (config b))) (board b))))) #-}
 
-{-# RULES "nextgen" [~] forall f1 f2. repBB (\b -> LifeBoard (config b) (board (absBB f1 b) ++ board (absBB f2 b))) = (\b -> LifeBoard (config b) (union (board (f1 b)) (board (f2 b)))) #-}
+{-# RULES "nextgen" [~] forall f1 f2 b. repB (LifeBoard (config (absB b)) (board (absB (f1 (repB (absB b)))) ++ board (absB (f2 (repB (absB b)))))) = LifeBoard (config b) (union (board (f1 b)) (board (f2 b))) #-}
 
-{-# RULES "next" [~] forall f. repBB (absBB f) = f #-}
+{-# RULES "next" [~] forall f b. repB (absB (f (repB (absB b)))) = f b #-}
 
 
